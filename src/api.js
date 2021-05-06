e3286576e99bf6dd1d8fcee567dc0dff549c2469
Nick <Nick.banken@Gmail.com>
2021-05-06 16:59:50 +0200

    created api requests and cards

---
+++ src/api.js
@@ -0,0 +1,48 @@
+//Base URL
+const base_url = "https://api.rawg.io/api/";
+const key = process.env.REACT_APP_GAMES;
+
+//Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+//Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//popular games
+const popular_games = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${key}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=${key}&dates=${lastYear},${currentDate}&odering=-released&page_size=10`;
+
+export const popularGamesURL = () => {
+  return `${base_url}${popular_games}`;
+};
+
+export const upcomingGamesURL = () => {
+  return `${base_url}${upcoming_games}`;
+};
+
+export const newGamesURL = () => {
+  return `${base_url}${new_games}`;
+};
